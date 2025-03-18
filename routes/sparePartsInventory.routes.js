73e20f3f8f0d0d872685d342c52dc14b5adcbe87
hashi-harindya <harindyahashini@gmail.com>
2025-03-19 00:33:33 +0530

    Implemet spare parts router Γëí╞Æ├ª├¼

---
+++ routes/sparePartsInventory.routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { addSpareParts } from "../controllers/sparePartsInventory.controller.js";
+const sparePartsInventoryRouter = express.Router();
+
+sparePartsInventoryRouter.post("/", addSpareParts);
+
+export default sparePartsInventoryRouter;
